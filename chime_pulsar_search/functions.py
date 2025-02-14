5ff18e230f5f31721593948c2310ed0f44da2369
Naman Jain <100242160+astronamanj@users.noreply.github.com>
2025-02-14 10:36:11 -0500

    Start CHIME Pulsar project

---
+++ chime_pulsar_search/functions.py
@@ -0,0 +1,8 @@
+import astropy
+import numpy as np  
+from astropy.io import fits
+from matplotlib import pyplot as plt  
+from matplotlib.colors import LogNorm, Normalize
+
+from rich.pretty import Pretty
+from sigpyproc.readers import FilReader
