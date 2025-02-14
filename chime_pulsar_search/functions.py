65ad1830ced19cd44f763de0778c3b655ae5784a
Naman Jain <100242160+astronamanj@users.noreply.github.com>
2025-02-14 14:59:30 -0500

    Done calibration (to be checked later)

--- chime_pulsar_search/functions.py
+++ chime_pulsar_search/functions.py
@@ -6,3 +6,12 @@ from matplotlib.colors import LogNorm, Normalize
 
 from rich.pretty import Pretty
 from sigpyproc.readers import FilReader
+
+def flux_density(
+        freq: float,
+        ref_freq: float = 400,
+        ref_flux: float = 8.684,
+        spectral_index: float = -0.6,
+)-> float:
+    """Calculate the flux density in Jy"""
+    return ref_flux * (freq / ref_freq) ** spectral_index
